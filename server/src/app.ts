f4d76672de1ee520b9efb13a686c850395f087f7
MurielDA <nana4six@tutamail.com>
2025-12-01 19:53:51 +0800

    api url change

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://miniblog-pied.vercel.app/'
+    ? 'https://miniblog-pied.vercel.app'
     : 'http://localhost:5173',
   credentials: true,
 }));
