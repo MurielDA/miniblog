ff87c38cda3272f3210ecd6646bdb1b272b9eb6b
MurielDA <nana4six@tutamail.com>
2025-12-01 19:39:30 +0800

    api url update

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { AuthResponse, PostsResponse } from "../types";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL+'/api' || 'http://localhost:3000/api';
 
 const api = axios.create({
     baseURL: API_URL,
